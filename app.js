fe953f5fb934d5b2c65af599a2aa6a6f6a515599
Evgeny Kasian <ekasian@outlook.com>
2018-08-30 15:55:09 -0700

    starting to work on auth

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let express = require("express");
 let path = require("path");
 let cookieParser = require("cookie-parser");
 let logger = require("morgan");
+const cors = require("cors");
 
 let indexRouter = require("./routes/index");
 let usersRouter = require("./routes/users");
@@ -9,6 +10,7 @@ let apiRouter = require("./routes/api");
 
 let app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
