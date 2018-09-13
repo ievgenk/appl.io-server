6bcda2732ffffb3d22d6be417841997bd635cb2b
Evgeny Kasian <ekasian@outlook.com>
2018-09-13 16:54:56 -0700

    added auth testing

--- tests/test-server.js
+++ tests/test-server.js
@@ -1,19 +1,71 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
+const mongoose = require("mongoose");
 
-const app = require("../app");
+const app = require("../server");
 
-const should = chai.should();
+const expect = chai.expect;
 chai.use(chaiHttp);
 
-describe("API", function() {
-  it("should 200 on GET requests", function() {
-    return chai
+describe("USER AUTH TESTING", function() {
+  before(async function() {
+    await mongoose.connect(
+      "mongodb://ekasian:gggggg1@ds035503.mlab.com:35503/applio-api-test"
+    );
+  });
+
+  it("Should register a new user", async function() {
+    let registeredUser = await chai
       .request(app)
-      .get("/api")
-      .then(function(res) {
-        res.should.have.status(200);
-        res.should.be.json;
+      .post("/users/signup")
+      .send({
+        email: "test@testing.ca",
+        password: "coolpass"
       });
+
+    expect(registeredUser).to.have.status(201);
+    expect(registeredUser.body.message).to.equal("User Created");
+  });
+
+  it("Should fail to register a new user", async function() {
+    let registeredUser = await chai
+      .request(app)
+      .post("/users/signup")
+      .send({
+        email: "test@testing.ca",
+        password: "1"
+      });
+
+    expect(registeredUser).to.have.status(500);
+  });
+
+  it("Should login registered user", async function() {
+    let loggedInUser = await chai
+      .request(app)
+      .post("/users/login")
+      .send({
+        email: "test@testing.ca",
+        password: "coolpass"
+      });
+
+    expect(loggedInUser.body).to.contain.keys(["token", "user"]);
+    expect(loggedInUser).to.have.status(200);
+  });
+
+  it("Should fail to login registered user", async function() {
+    let loggedInUser = await chai
+      .request(app)
+      .post("/users/login")
+      .send({
+        email: "test@testing.ca",
+        password: "1"
+      });
+
+    expect(loggedInUser).to.have.status(401);
+  });
+
+  after(async function() {
+    await mongoose.connection.dropCollection("users");
+    await mongoose.disconnect();
   });
 });
